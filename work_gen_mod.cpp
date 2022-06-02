3528cdeef48e69b98ac4e9ea0b3752c76395e158
Aneesh Raman <aneeshr@bu.edu>
2022-06-02 14:29:02 -0400

    code cleanup

--- work_gen_mod.cpp
+++ work_gen_mod.cpp
@@ -21,34 +21,6 @@ void generate_one_file(unsigned long pTOTAL_NUMBERS, int k, int l, int pseed, st
 unsigned int get_number_domain(unsigned long position, unsigned long total, unsigned long domain_);
 std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L, int seed, std::string folder, std::string type, double alpha, double beta, int payload_size);
 
-inline void showProgress(const long &n, const long &count);
-inline void showProgress(const long &workload_size, const long &counter)
-{
-    // std::cout << “flag = ” << flag << std::endl;
-    // std::cout<<“2----“;
-    if (counter / (workload_size / 100) >= 1)
-    {
-        for (int i = 0; i < 104; i++)
-        {
-            std::cout << "\b";
-            fflush(stdout);
-        }
-    }
-    for (int i = 0; i < counter / (workload_size / 100); i++)
-    {
-        std::cout << "=";
-        fflush(stdout);
-    }
-    std::cout << std::setfill(' ') << std::setw(101 - counter / (workload_size / 100));
-    std::cout << counter * 100 / workload_size << "%";
-    fflush(stdout);
-    if (counter == workload_size)
-    {
-        std::cout << "\n";
-        return;
-    }
-}
-
 std::string generateValue(int value_size)
 {
     std::string value = std::string(value_size, 'a' + (rand() % 26));
@@ -61,31 +33,6 @@ inline bool ledger_exists()
     return f.good();
 }
 
-int sample_beta()
-{
-    double alpha = 2, beta = 3;
-    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    std::default_random_engine e(seed);
-
-    std::uniform_real_distribution<double> distr(0, 1);
-    // double randFromUnif = distr(e);
-
-    double randFromUnif = ((double)rand() / (RAND_MAX));
-
-    beta_distribution<> dist(alpha, beta);
-    double randFromDist = quantile(dist, randFromUnif);
-
-    std::cout << "distr = " << randFromDist << "\t";
-
-    int m = -100;
-    int M = 100;
-
-    int y = m + (M - m) * randFromDist;
-
-    std::cout << "conv = " << y << std::endl;
-
-    return randFromDist;
-}
 
 void generate_one_file(unsigned long pTOTAL_NUMBERS, int k, int l, int pseed, std::string folder, std::string type, double alpha, double beta, int payload_size)
 {
@@ -94,15 +41,10 @@ void generate_one_file(unsigned long pTOTAL_NUMBERS, int k, int l, int pseed, st
     srand(time(NULL));
     outfile.open("dataledger.txt", std::ios_base::app);
 
-    // std::string folder_name = "mixed_workload/";
     std::string folder_name = "./";
-    // std::string folder_name = "vary_buffer_workload/";
-    //    std::string folder_name = "sorting_workload/";
+
     outfile << generate_partitions_stream(pTOTAL_NUMBERS, k, l, pseed, folder, type, alpha, beta, payload_size) << std::endl;
-    // for (int i = 0; i < 10; i++)
-    // {
-    //     sample_beta();
-    // }
+
     outfile.close();
 }
 
@@ -175,118 +117,7 @@ unsigned long generate_beta_random_in_range(long position, unsigned long Total_N
     return ret;
 }
 
-unsigned long generate_random_in_range(unsigned long position, unsigned long Total_Numbers, int L)
-{
-    int l = L;
-
-    long ret;
-
-    uint min_jump = 1;
-    uint max_jump = l;
-    // std::cout << "Max jump possible = " << max_jump << std::endl;
-
-    // a jump can be only of l windowSizeaces
-    // int jump = rand() % (max_jump) + min_jump;
-    unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
-    std::default_random_engine e(seed);
-    std::uniform_int_distribution<unsigned long> distr(min_jump, max_jump); // this is inclusive on both ends
-    unsigned long jump = distr(e);
-
-    // now lets do a small check. If both jumping forward and backward is invalid,
-    // we need to reconfigure the jump to the max limit.
-    // note, if position < jump, position-jump < 0
-    if ((position + jump) >= Total_Numbers && (position <= jump))
-    {
-        // we can pick both ends now to determine the max jump
-        // lets do a coin toss
-        float p = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-        if (p < 0.5)
-        {
-            // max jump configured upto position
-            max_jump = position - 1;
-        }
-        else
-        {
-            max_jump = Total_Numbers - (position - 1) - 1;
-        }
-
-        // redo jump
-        jump = rand() % (max_jump) + min_jump;
-    }
-
-    if (position <= l && ((position + jump) <= Total_Numbers))
-    {
-        // ret=rand() % position+l;
-        // return ((unsigned long)rand() % position+l);
-
-        // we can only jump forward
-        ret = position + jump;
-        if (ret < 0)
-        {
-            std::cout << "oops (negative index)" << std::endl;
-            std::cout << "jump = " << jump << "; ret = " << ret << "; position = " << position << std::endl;
-        }
-        if (ret > Total_Numbers)
-        {
-            std::cout << "oops overboard" << std::endl;
-        }
-    }
-    else if ((unsigned long)(position + l) >= Total_Numbers)
-    {
-        // ret = rand() % (Total_Numbers - (position - l)) + (position - l);
-        // return ((unsigned long)rand() % (Total_Numbers - (position - l))) + (position - l);
-
-        // we can only jump backward
-        ret = position - jump;
-        if (ret < 0)
-        {
-            std::cout << "oops (negative index)" << std::endl;
-            std::cout << "jump = " << jump << "; ret = " << ret << "; position = " << position << std::endl;
-        }
-        if (ret > Total_Numbers)
-        {
-            std::cout << "oops overboard" << std::endl;
-        }
-    }
-    else
-    {
-        // ret = rand() % ((position + l) - (position - l)) + (position - l);
-        // return ((unsigned long)rand() % ((position + l) - (position - l))) + (position - l);
-
-        // we can jump forward or backward
-        // let's toss a coin to find out what to do
-        float p = static_cast<float>(rand()) / static_cast<float>(RAND_MAX);
-        // we move backwards with p < 0.5
-        if (p < 0.5)
-            ret = position - jump;
-        else
-            ret = position + jump;
-
-        if (ret < 0)
-        {
-            std::cout << "oops (negative index)" << std::endl;
-            std::cout << "jump = " << jump << "; ret = " << ret << "; position = " << position << std::endl;
-        }
-        if (ret > Total_Numbers)
-        {
-            std::cout << "oops overboard" << std::endl;
-        }
-    }
-    if (ret < 0)
-    {
-        std::cout << "oops (negative index)" << std::endl;
-        std::cout << "jump = " << jump << "; ret = " << ret << "; position = " << position << std::endl;
-    }
-
-    if (ret > Total_Numbers)
-    {
-        std::cout << "oops overboard" << std::endl;
-    }
-
-    return (unsigned long)ret;
-}
-
-// code inspired from GeekForGeeks
+// this function code from GeekForGeeks
 double findMedian(std::vector<long> a,
                   int n)
 {
@@ -335,13 +166,12 @@ double findMedian(std::vector<long> a,
     */
 std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L, int seed, std::string folder = "./Data", std::string type = "bin", double alpha = 1.0, double beta = 1.0, int payload_size = 252)
 {
-    // float p_outOfRange = (double)K / TOTAL_NUMBERS;
+
     float p_outOfRange = K;
 
     std::srand(seed);
 
-    // unsigned long array[TOTAL_NUMBERS];
-    //  std::vector<unsigned long> array(TOTAL_NUMBERS, 0);
+
     unsigned long *array = new unsigned long[TOTAL_NUMBERS];
 
     std::string f1name = folder;
@@ -357,30 +187,16 @@ std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L
     f1name += std::to_string(std::time(nullptr));
 
     std::ofstream myfile1;
-    // f1name += ".dat";
-    // if (type.compare("txt") == 0)
-    // {
-    //     f1name += ".txt";
-    //     myfile1.open(f1name);
-    // }
 
-    // else
-    // {
-    //     f1name += ".dat";
-    //     myfile1.open(f1name, std::ios::binary);
-    // }
 
     f1name += ".txt";
     myfile1.open(f1name);
 
-    // std::ofstream myfile(fname, std::ios::binary);
 
-    // if (type.compare("txt") == 0)
-    //     myfile1(f1name);
 
     unsigned long noise_limit = TOTAL_NUMBERS * p_outOfRange / 100.0;
     unsigned long l_absolute = TOTAL_NUMBERS * L / 100.0;
-    // assert(noise_limit == K);
+
     unsigned long noise_counter = 0;
 
     std::unordered_set<unsigned long> myset;
@@ -388,7 +204,7 @@ std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L
     std::unordered_set<unsigned long> left_out_sources;
     unsigned long w = 0;
     unsigned long windowSize = 1;
-    // std::cout << "pOut = " << p_outOfRange << std::endl;
+
     std::cout << "L% = " << L << std::endl;
     std::cout << "L absolute = " << l_absolute << std::endl;
     std::cout << "No. of Required Swaps = " << noise_limit << std::endl;
@@ -477,10 +293,7 @@ std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L
 
     // since the first source has already been taken care of, simply start from the next
     auto itr = myset.begin();
-    // std::cout << "itr = " << *itr << std::endl;
-    // ++itr;
-    // std::cout << "next = " << *itr << std::endl;
-    // now, loop through the sources values
+
     for (; itr != myset.end(); itr++)
     {
         unsigned long r;
@@ -703,28 +516,6 @@ std::string generate_partitions_stream(unsigned long TOTAL_NUMBERS, int K, int L
 
     double median_l = findMedian(l_values, l_values.size());
     std::cout << "Median L = " << median_l << std::endl;
-
-    // if (type.compare("txt") == 0)
-    // {
-    //     for (unsigned long j = 0; j < TOTAL_NUMBERS; ++j)
-    //     {
-    //         // auto pair = std::make_pair(array[j], array[j]);
-    //         // //myfile.write(reinterpret_cast<char *>(&pair), sizeof(std::pair<unsigned int, unsigned int>));
-    //         // myfile1.write(reinterpret_cast<char *>(&array[j]), sizeof(int));
-    //         myfile1 << array[j] << ",";
-    //     }
-    // }
-    // else
-    // {
-    //     for (unsigned long j = 0; j < TOTAL_NUMBERS; ++j)
-    //     {
-    //         // auto pair = std::make_pair(array[j], array[j]);
-    //         // myfile.write(reinterpret_cast<char *>(&pair), sizeof(std::pair<unsigned int, unsigned int>));
-    //         myfile1.write(reinterpret_cast<char *>(&array[j]), sizeof(unsigned long));
-    //         // std::cout<<array[j]<<std::endl;
-    //         // break;
-    //     }
-    // }
 
     for (unsigned long j = 0; j < TOTAL_NUMBERS; j++)
     {
