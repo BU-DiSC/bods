62094e619bfd5e49d0975de0010ef3d76211c7a4
Konstantinos Karatsenidis <karatse@bu.edu>
2022-06-10 01:06:35 +0300

    runloads.sh changes

--- runloads.sh
+++ runloads.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-export DB=MONETDB
-export N=16000000
 export OUT_CSV=results.csv
-K=0 L=5 ./benchmark.sh 4 80 3200000
-K=1 L=1 ./benchmark.sh 4 80 3200000
-K=5 L=5 ./benchmark.sh 4 80 3200000
-K=10 L=10 ./benchmark.sh 4 80 3200000
-K=25 L=25 ./benchmark.sh 4 80 3200000
-K=50 L=50 ./benchmark.sh 4 80 3200000
-K=100 L=100 ./benchmark.sh 4 80 3200000
+export DB=MYSQL
+export N=16000000
+{
+  K=0 L=5 ./benchmark.sh 4 80 3200000
+  K=1 L=1 ./benchmark.sh 4 80 3200000
+  K=5 L=5 ./benchmark.sh 4 80 3200000
+  K=10 L=10 ./benchmark.sh 4 80 3200000
+  K=25 L=25 ./benchmark.sh 4 80 3200000
+  K=50 L=50 ./benchmark.sh 4 80 3200000
+  K=100 L=100 ./benchmark.sh 4 80 3200000
+} >>$OUT_CSV
