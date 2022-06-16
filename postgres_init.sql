f5b502b450cec34faac90e6ef4edb0132a3aa130
Konstantinos Karatsenidis <karatse@bu.edu>
2022-06-16 04:33:22 +0300

    postgres_init.sql unlogged table

--- postgres_init.sql
+++ postgres_init.sql
@@ -1,4 +1,4 @@
 DROP INDEX IF EXISTS id_index;
 DROP TABLE IF EXISTS test_table;
-CREATE TABLE test_table (id_col INT, payload VARCHAR(252));
+CREATE UNLOGGED TABLE test_table (id_col INT, payload VARCHAR(252));
 CREATE UNIQUE INDEX id_index ON test_table USING btree (id_col);
